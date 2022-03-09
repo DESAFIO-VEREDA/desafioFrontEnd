6ff2c21c827a9cff00686d2e6c4123bdd8537909
Rafael Torreson <vi_guepardo@hotmail.com>
2022-03-09 11:16:48 -0300

    styleCard

---
+++ pokedex/src/App.js
@@ -0,0 +1,28 @@
+import './styles/home.scss'
+
+export function App() {
+  return (
+    <div className="App">
+      <div className="Cards">
+        <div className="Card">
+
+          <div>
+            <h1>Bulbasaur</h1>
+            <p className='Properties'>Grass</p>
+            <p className='Properties'>Poison</p>
+          </div> 
+          <div className='imgBackground'>
+            <div>
+              <div className='id'>
+                <p>#001</p>
+              </div>
+             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="Ilustração Bulbasaur" />  
+            </div>
+          </div> 
+        </div>  
+      </div>
+    </div>
+  );
+}
+
+export default App;
